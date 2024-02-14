ea862d34dbc04d6a6e4db900c8226ba610f7817e
jodemustafa <111262084+jodemustafa@users.noreply.github.com>
2024-02-13 21:49:28 -0500

    fhyfygj

--- NotationConvertor.cpp
+++ NotationConvertor.cpp
@@ -2,7 +2,29 @@
 #include <string>
 #include "NotationConvertor.hpp"
 
-int main(){
+int main()
+{
 
     return 1;
+}
+
+int fg = 0;
+int NotationConvertor::getPrecedence(char c)
+{
+    if (c == '+' || c == '-')
+    {
+        return 1;
+    }
+    else if (c == '*' || c == '/')
+    {
+        return 2;
+    }
+    else if (c == '^')
+    {
+        return 3;
+    }
+    else
+    {
+        return -1;
+    }
 }
